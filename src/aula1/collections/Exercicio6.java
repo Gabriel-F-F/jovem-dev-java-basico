f00e24abc6f94e36b9a1548c6388014919278a70
Gabriel <gfelixfaustina@gmail.com>
2024-07-30 12:44:18 -0300

    ExerciciosHome

--- src/aula1/collections/Exercicio6.java
+++ src/aula1/collections/Exercicio6.java
@@ -1,5 +1,35 @@
 package aula1.collections;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JOptionPane;
+
 public class Exercicio6 {
 
+	public static void main(String[] args) {
+
+		new Exercicio6();
+	}
+	
+	public Exercicio6() {
+		
+		Map<String, List<Double>> mapaAluno = new HashMap<>();
+		String nome;
+		List<Double> nota = null;
+		
+		for(int i = 0; i < 2; i++) {
+			
+			nome = JOptionPane.showInputDialog("Nome do Aluno: ");
+			
+			for(int j = 0; j < 3; j++) {
+				nota.add(Double.parseDouble(JOptionPane.showInputDialog("Nota " + (j + 1))));
+			
+			}
+			mapaAluno.put(nome, nota);
+		}
+		
+		System.out.println(mapaAluno);
+	}
 }
