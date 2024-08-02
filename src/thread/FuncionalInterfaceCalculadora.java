87fbead71b02934815306d09c9164d147e7e6548
Gabriel <gfelixfaustina@gmail.com>
2024-08-02 01:41:36 -0300

    teste

--- src/thread/FuncionalInterfaceCalculadora.java
+++ src/thread/FuncionalInterfaceCalculadora.java
@@ -1,8 +1,8 @@
 package thread;
 
-@FunctionalInterface
 public class FuncionalInterfaceCalculadora {
 
-	public void calcula(double valor1, double valor2) {
+	public double calcula(double valor1, double valor2) {
+		return valor1 * valor2;
 	}
 }
