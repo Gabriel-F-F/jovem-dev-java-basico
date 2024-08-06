6cca3103dab5355b82819837aead2d238f023b60
Gabriel-F-F <gfelixfaustina@gmail.com>
2024-08-06 17:28:25 -0300

    ExercicicioEnumString

--- src/aula6/outros/CalculoInterface.java
+++ src/aula6/outros/CalculoInterface.java
@@ -1,22 +1,27 @@
 package aula6.outros;
 
-interface CalculoInterface {
+@FunctionalInterface
+public interface CalculoInterface {
 
-	public abstract class CalculoInterfaceFuncional  {
-		public int calcula(int x, int y, int escolha) {
-			if (escolha == 1) {
-				escolha = x + y;
-			}
-			if (escolha == 2) {
-				escolha = x - y;
-			}
-			if (escolha == 3) {
-				escolha = x * y;
-			}
-			if (escolha == 4) {
-				escolha = x / y;
-			}
-			return escolha;
+	int calcula();
+}
+
+public abstract class CalculoInterfaceFuncional {
+	public int calcula(int x, int y, int escolha) {
+		if (escolha == 1) {
+			escolha = x + y;
 		}
+		if (escolha == 2) {
+			escolha = x - y;
+		}
+		if (escolha == 3) {
+			escolha = x * y;
+		}
+		if (escolha == 4) {
+			escolha = x / y;
+		}
+		return escolha;
 	}
 }
+
+//https://medium.com/@mvalho/java-8-functional-interfaces-tornando-o-java-mais-legal-72401462d0e2
