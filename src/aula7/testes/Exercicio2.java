75fce3a6709682534f8fdf93ccaec507888ddc75
Gabriel <gfelixfaustina@gmail.com>
2024-08-08 12:00:54 -0300

    exercicioHome

--- src/aula7/testes/Exercicio2.java
+++ src/aula7/testes/Exercicio2.java
@@ -2,6 +2,16 @@ package aula7.testes;
 
 public class Exercicio2 {
 
-	
+//	Altere o Exercicio 5 da aula de Optional/Stream para
+//	criar um método que recebe uma lista de String e
+//	retorna uma String, disparando ZeroBertoException
+//
+//	caso não encontre um “berto”.
+//
+//	Crie um arquivo de testes para verificar se esse
+//	método se comporta corretamente.
+//	● Lista vazia, lista sem bertos: Exception
+//	● Lista com um berto: Esse elemento
+//	● Lista com dois bertos: O primeiro berto	
 	
 }
